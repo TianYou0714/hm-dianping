5df3b3e41626277043d97a27f2ce3b041b724a55
TianYou <tianyou0714@qq.com>
2026-05-28 23:20:04 +0800

    电话号码验证发送短信

---
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package com.hmdp.service.impl;
+
+import cn.hutool.core.util.RandomUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.User;
+import com.hmdp.mapper.UserMapper;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Slf4j
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    /**
+     * 发送验证码
+     * @param phone 手机号
+     * @return 验证码发送结果
+     */
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        //校验手机号
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            //如果不符合返回错误信息
+            return Result.fail("手机号格式错误");
+        }
+
+        //符合，生成验证码
+        String code = RandomUtil.randomNumbers(6);
+
+        //保存验证码到session
+        session.setAttribute("code", code);
+
+        //发生验证码
+        log.debug("发送验证码成功！验证码为：{}", code);
+        //返回结果
+        return Result.ok();
+    }
+}

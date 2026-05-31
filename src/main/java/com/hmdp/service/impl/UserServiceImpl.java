9de0b85d78f8ff2d42476a3cc11cf99d6682474d
TianYou <tianyou0714@qq.com>
2026-05-31 21:45:15 +0800

    修复Redis序列化Long转String类型错误，添加登录拦截器和全局配置

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,16 +1,32 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+
+import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -23,6 +39,10 @@ import javax.servlet.http.HttpSession;
 @Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
 
     /**
      * 发送验证码
@@ -40,12 +60,78 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         //符合，生成验证码
         String code = RandomUtil.randomNumbers(6);
 
-        //保存验证码到session
-        session.setAttribute("code", code);
+        //保存验证码到redis
+//        session.setAttribute("code", code);
+        stringRedisTemplate.opsForValue().set(LOGIN_CODE_KEY + phone, code, LOGIN_CODE_TTL, TimeUnit.MINUTES);
 
         //发生验证码
         log.debug("发送验证码成功！验证码为：{}", code);
         //返回结果
         return Result.ok();
+    }
+    /**
+     * 登录功能
+     * @param loginForm 登录参数，包含手机号、验证码；或者手机号、密码
+     */
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        //校验手机号
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            //如果不符合返回错误信息
+            return Result.fail("手机号格式错误");
+        }
+
+        // 从redis校验验证码
+        String cacheCode = stringRedisTemplate.opsForValue().get(LOGIN_CODE_KEY + phone);
+        String code = loginForm.getCode();
+
+        if (cacheCode == null || !cacheCode.equals(code)){
+            //不一致，报错
+            return Result.fail("验证码错误");
+        }
+
+        //一致，根据手机查询用户
+        User user = query().eq("phone", phone).one();
+
+        //判断用户是否存在
+        if (user == null){
+            //不存在，创建新用户并保存
+            user = createUserWithPhone(phone);
+        }
+
+        //保存用户在redis中
+        //-随机生成token作为登录令牌
+        String token = UUID.randomUUID().toString(true);
+
+        //-将user对象转为hashmap保存到redis中
+        UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
+        Map<String, Object> userMap = BeanUtil.beanToMap(userDTO, new HashMap<>(),
+                CopyOptions.create()
+                        .setIgnoreNullValue(true)
+                        .setFieldValueEditor((fieldName, fieldValue) -> fieldValue.toString()));
+
+        //-保存
+        String tokenKey = LOGIN_USER_KEY + token;
+        stringRedisTemplate.opsForHash().putAll(tokenKey, userMap);
+
+        //-设置token有效期
+        stringRedisTemplate.expire(tokenKey, LOGIN_USER_TTL, TimeUnit.MINUTES);
+        //返回token
+        return Result.ok(token);
+    }
+
+    /**
+     * 创建新用户
+     * @param phone 手机号
+     * @return 新用户
+     */
+    private User createUserWithPhone(String phone) {
+        //创建新用户
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+        save(user);
+        return user;
     }
 }
---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,63 @@
+package com.hmdp.utils;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    private StringRedisTemplate stringRedisTemplate;
+
+    public LoginInterceptor(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //1.获取请求头中的token
+        String token = request.getHeader("authorization");
+        if (StrUtil.isBlank(token)) {
+            //不存在，拦截
+            response.setStatus(401);
+            return false;
+        }
+
+        //2.基于token获取redis中的用户
+        String key = RedisConstants.LOGIN_USER_KEY;
+        Map<Object, Object> userMap = stringRedisTemplate.opsForHash().entries(key + token);
+
+        //判断用户是否存在
+        if (userMap.isEmpty()) {
+            //不存在，拦截
+            response.setStatus(401);
+            return false;
+        }
+        //将查询到的hash数据转为userDTO
+        UserDTO userDTO = BeanUtil.fillBeanWithMap(userMap, new UserDTO(), false);
+
+        //存在，保存用户信息在ThreadLocal
+        UserHolder.saveUser(userDTO);
+
+        //刷新token有效期
+        stringRedisTemplate.expire(key, RedisConstants.LOGIN_USER_TTL, TimeUnit.MINUTES);
+        //放行
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+        //移除用户
+        UserHolder.removeUser();
+    }
+}
